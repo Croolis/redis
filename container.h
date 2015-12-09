7ac20d5372448cf315056269f9d48474a08dbe1f
Givorenon <qwertyqwerty05@list.ru>
2015-12-09 17:24:54 +0300

    simple persistance by writing 'set' commands in log and loading from log on start of server

--- container.h
+++ container.h
@@ -2,6 +2,11 @@
 #include <set>
 #include <unordered_map>
 #include <time.h>
+#include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -10,11 +15,74 @@ class container {
     unordered_map<string, string> container_;
     unordered_map<string, time_t> ttl_;
     set<pair<string, time_t> > ttl_set_;
+    ofstream log;
 
  public:
+    container() {
+        string path = "log";
+        int fd = open(path.c_str(), O_RDONLY);
+        if (fd == -1) {
+            cout << "Can not open log\n";
+        } else {
+            char str[4096];
+            int len = 0, p = -1, num = 0, condition = 0;
+            string key, value;
+            while (true) {
+                p++;
+                if (p == len) {
+                    len = read(fd, str, sizeof(str));
+                    if (len < 0)
+                        cout << "Can not open log\n";
+                    p = 0;
+                }
+                if (len <= 0)
+                    break;
+                if (condition == 0) {
+                    container_[key] = value;
+                    key = "";
+                    value = "";
+                }
+                if (condition == 0 || condition == 2) {
+                    if (str[p] == '$') {
+                        condition = (condition + 1) % 4;
+                        continue;
+                    }
+                    else {
+                        num *= 10;
+                        num += str[p] - '0';
+                    }           
+                }
+                if (condition == 1) {
+                    key += str[p];
+                    num--;
+                    if (num == 0) {
+                        condition = (condition + 1) % 4;
+                        continue;
+                    }
+                }
+                if (condition == 3) {
+                    value += str[p];
+                    num--;
+                    if (num == 0) {
+                        condition = (condition + 1) % 4;
+                        continue;
+                    }
+                }
+            }
+            close(fd);
+        }
+        log.open("log");
+    }
+
+    ~container() {
+        log.close();
+    }
+
     void set_val(string key, string value) {
         cout << "in container.set " << key << ' ' << value << endl;
         container_[key] = value;
+        log << key.length() << '$' << key << value.length() << '$' << value;
+        log.flush();
         ttl_.erase(key);
         auto it = ttl_set_.lower_bound(make_pair(key, 0));
         if (it != ttl_set_.end())
